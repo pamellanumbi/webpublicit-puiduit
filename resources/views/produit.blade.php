1669f90a55380d463443ce0816ee23240a95e20b
Pamella <pamellanumbi@gmail.com>
2021-04-16 20:25:14 +0200

    Creation de la page d'ajout du produit

--- resources/views/produit.blade.php
+++ resources/views/produit.blade.php
@@ -11,11 +11,30 @@
     <title>Hello, world!</title>
 </head>
 <body>
-<h1>Hello, world! STeven</h1>
+    @if($layout=='index')
+        <div class="container-fluid">
+            <div class="row">
+                <section class="col">@include('produitList')</section>
+                <section class="col"></section>
+            </div>
 
-<!-- Optional JavaScript; choose one of the two! -->
-
-<!-- Option 1: Bootstrap Bundle with Popper -->
+        </div>
+    @elseif($layout=='create')
+        <div class="container-fluid">
+            <section class="col">@include('produitList')</section>
+            <section class="col"></section>
+        </div>
+    @elseif($layout=='show')
+        <div class="container-fluid">
+            <section class="col">@include('produitList')</section>
+            <section class="col"></section>
+        </div>
+    @elseif($layout=='edit')
+        <div class="container-fluid">
+            <section class="col">@include('produitList')</section>
+            <section class="col"></section>
+        </div>
+    @endif
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
 
 <!-- Option 2: Separate Popper and Bootstrap JS -->
---
+++ resources/views/produitList.blade.php
@@ -0,0 +1,31 @@
+<div class="col">
+    <table class="table table-striped">
+        <thead>
+        <tr>
+            <th scope="col">N°</th>
+            <th scope="col">Code</th>
+            <th scope="col">Designation</th>
+            <th scope="col">Description</th>
+            <th scope="col">Description</th>
+            <th scope="col">PU</th>
+            <th scope="col">Actions</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($produits as $p)
+        <tr>
+            <th scope="row">{{ $p->id }}</th>
+            <td>{{ $p->codeProd }}</td>
+            <td>{{ $p->designation }}</td>
+            <td>{{ $p->description }}</td>
+            <td>{{ $p->prix }}</td>
+            <td>
+                <a href="{{ url('/show/'.$p->id) }}" class="btn btn-sm btn-info">Afficher</a>
+                <a href="{{ url('/edit/'.$p->id) }}" class="btn btn-sm btn-warning">Modifier</a>
+                <a href="{{ url('/destroy/'.$p->id) }}" class="btn btn-sm btn-danger">Supprimer</a>
+            </td>
+        </tr>
+        @endforeach
+        </tbody>
+    </table>
+</div>
